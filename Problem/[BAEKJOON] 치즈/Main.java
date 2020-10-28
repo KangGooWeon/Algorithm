d518c82096061b345c7e5b198defb835c927ec2a
GoldenEastLight <been0874@naver.com>
2020-10-28 13:20:48 +0900

    커밋

---
+++ Problem/[BAEKJOON] 치즈/Main.java
@@ -0,0 +1,93 @@
+﻿import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[][] map;
+	static int time, tempCh, cheese;
+	static int N,M;
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {1,-1,0,0};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		
+		map = new int[N+2][M+2];
+		
+		for(int i =1; i<=N; i++) {
+			st = new StringTokenizer(br.readLine());
+			for(int j =1; j<=M; j++) {
+				map[i][j] = Integer.parseInt(st.nextToken());
+				if (map[i][j] == 1) cheese++;
+			}
+		}
+		
+		while(true) {
+			time++;
+			int[][] remove = new int[N+2][M+2];
+			remove = bfs(remove);
+			tempCh = cheese;
+			removeCheese(remove);
+			if(cheese <=0 )break;
+		}
+		
+		System.out.println(time);
+		System.out.println(tempCh);
+	}
+	private static void removeCheese(int[][] remove) {
+		for(int i =1; i<=N; i++) {
+			for(int j =1; j<=M; j++) {
+				if(remove[i][j] == 1) {
+					map[i][j] = 0;
+					cheese--;
+				}
+			}
+		}
+	}
+	private static int[][] bfs(int[][] remove) {
+		Queue<Pos> qu = new LinkedList<>();
+		qu.offer(new Pos(0,0));
+		boolean[][] visited = new boolean[N+2][M+2];
+		visited[0][0] = true;;
+		
+		while(!qu.isEmpty()) {
+			int size = qu.size();
+			for(int i =0; i<size; i++) {
+				Pos pos = qu.poll();
+				for(int k =0; k<4; k++) {
+					int x = pos.x + dx[k];
+					int y = pos.y + dy[k];
+					
+					if(x <0 || x>N || y <0 || y>M || visited[x][y]) continue;
+					
+					if(map[x][y] == 1 ) {
+						remove[x][y] = 1;
+						continue;
+					}
+					
+					qu.offer(new Pos(x,y));
+					visited[x][y] = true;;
+				}
+			}
+		}
+		
+		return remove;
+	}
+	
+	static class Pos{
+		int x;
+		int y;
+		public Pos(int x, int y) {
+			super();
+			this.x = x;
+			this.y = y;
+		}
+		
+	}
+}
---
+++ Problem/[SW EXpert] 추억의2048게임/Solution.java
@@ -0,0 +1,157 @@
+﻿
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	static int N, DIS;
+	static int[][] map;
+	static int[] dx = {0,0,-1,1};
+	static int[] dy = {1,-1,0,0};
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int tc = Integer.parseInt(br.readLine());
+		
+		for(int t =1; t<=tc; t++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			String s = st.nextToken();
+			map = new int[N][N];
+			
+			for(int i =0; i<N; i++) {
+				st = new StringTokenizer(br.readLine());
+				for(int j=0; j<N; j++) {
+					map[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+			
+			if(s.equals("right")) {
+				for(int i = 0; i< N; i++) {
+					boolean[] visited = new boolean[N];
+					for(int j =N-2; j>=0; j--) {
+						int x = j;
+						while(true) {
+							if (map[i][x] == 0) break;
+							if (x >= N-1 || visited[x+1]) break;
+							
+							if (map[i][x+1] == 0) {
+								map[i][x+1] = map[i][x];
+								map[i][x] =0;
+								x++;
+								continue;
+							}
+							
+							if (map[i][x+1] == map[i][x]) {
+								map[i][x] = 0;
+								map[i][x+1] *=2;
+								visited[x+1] = true;
+								break;
+							}else {
+								break;
+							}
+						}
+					}
+				}
+			}
+			else if(s.equals("left")) {
+				for(int i = 0; i< N; i++) {
+					boolean[] visited = new boolean[N];
+					for(int j =1; j<N; j++) {
+						int x = j;
+						while(true) {
+							if (map[i][x] == 0) break;
+							if (x <= 0 || visited[x-1]) break;
+							
+							if (map[i][x-1] == 0) {
+								map[i][x-1] = map[i][x];
+								map[i][x] =0;
+								x--;
+								continue;
+							}
+							
+							if (map[i][x-1] == map[i][x]) {
+								map[i][x] = 0;
+								map[i][x-1] *=2;
+								visited[x-1] = true;
+								break;
+							}else {
+								break;
+							}
+						}
+					}
+				}
+			}
+			else if(s.equals("up")) {
+				for(int i = 0; i< N; i++) {
+					boolean[] visited = new boolean[N];
+					for(int j =1; j<N; j++) {
+						int x = j;
+						while(true) {
+							if (map[x][i] == 0) break;
+							if (x <= 0 || visited[x-1]) break;
+							
+							if (map[x-1][i] == 0) {
+								map[x-1][i] = map[x][i];
+								map[x][i] =0;
+								x--;
+								continue;
+							}
+							
+							if (map[x-1][i] == map[x][i]) {
+								map[x][i] = 0;
+								map[x-1][i] *=2;
+								visited[x-1] = true;
+								break;
+							}else {
+								break;
+							}
+						}
+					}
+				}
+			}
+			else if(s.equals("down")) { 
+				for(int i = 0; i< N; i++) {
+					boolean[] visited = new boolean[N];
+					for(int j =N-2; j>=0; j--) {
+						int x = j;
+						while(true) {
+							if (map[x][i] == 0) break;
+							if (x >= N-1 || visited[x+1]) break;
+							
+							if (map[x+1][i] == 0) {
+								map[x+1][i] = map[x][i];
+								map[x][i] =0;
+								x++;
+								continue;
+							}
+							
+							if (map[x+1][i] == map[x][i]) {
+								map[x][i] = 0;
+								map[x+1][i] *=2;
+								visited[x+1] = true;
+								break;
+							}else {
+								break;
+							}
+						}
+					}
+				}
+			}
+			
+			
+			// 출력
+			StringBuffer sb = new StringBuffer();
+			for(int i =0; i<N; i++) {
+				for(int j=0; j<N; j++) {
+					sb.append(map[i][j] + " ");
+				}
+				if(i!= N-1)sb.append("\n");
+			}
+			
+			System.out.println("#"+t);
+			System.out.println(sb.toString());
+		}
+	}
+}
