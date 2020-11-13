d4a5f80a7fb287af8a0c04a2fdcfe9b7cff923d7
GooWeonKang <fkdldjs487@gmail.com>
2020-11-13 23:51:42 +0900

    2018 KAKAO BLIND RECRUITMENT 레벨2

--- Problem/[Programmers] 프렌즈4블록/Solution.java
+++ Problem/[Programmers] 프렌즈4블록/Solution.java
@@ -1,68 +1,85 @@
-import java.util.Arrays;
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
-	public int solution(int m, int n, String[] board) {
-		int answer = 0;
-		char[][] game = new char[m][n];
-		char[][] map = new char[m][n];
-		char[] moveArr = new char[m];
-		boolean flag = false;
-		
-		for(int i=0; i<m; i++) {
-			for(int j=0; j<n; j++) {
-				game[i][j] = board[i].charAt(j);
-			}
-		}
-		
-		while(true) {
-			for(int i=0; i<m-1; i++) {
-				for(int j=0; j<n-1; j++) {
-					int cnt =0;
-					if(game[i][j]!='^') {
-						if(game[i][j] == game[i+1][j]) cnt++;
-						if(game[i][j] == game[i][j+1]) cnt++;
-						if(game[i][j] == game[i+1][j+1]) cnt++;
-						
-						if(cnt == 3) {
-							flag = true;
-							map[i][j] = '^';
-							map[i+1][j] = '^';
-							map[i][j+1] = '^';
-							map[i+1][j+1] = '^';
-						}
-					}
-				}
-			}
-			
-			if(!flag) break;
-			
-			for(int i=0; i<n; i++) {
-				int index =0;
-				for(int j=m-1; j>=0; j--) {
-					if(map[j][i] == '^') {
-						answer++;
-						continue;
-					}
-					
-					moveArr[index++] = game[j][i];
-				}
-				
-				int ind =0;
-				for(int j=m-1; j>=0; j--) {
-					if( ind < index) {
-						game[j][i] = moveArr[ind++];
-					}
-					else {
-						game[j][i] = '^';
-					}
-				}
-			}
-			
-			for(int i=0; i<m; i++) {
-				Arrays.fill(map[i], '>');
-			}
-			flag= false;
-		}
-		
-		return answer;
-	}
+    char[][] map;
+    boolean[][] visited;
+    int N, M, cnt;
+    
+    public int solution(int m, int n, String[] board) {
+        int answer = 0;
+        map = new char[m][n];
+        N = n;
+        M = m;
+        
+        for(int i =0; i<m; i++){
+            char[] c = board[i].toCharArray();
+            for(int j=0; j<n; j++){
+                map[i][j] = c[j];
+            }
+        }
+        
+        while(true){
+            boolean ch = false;
+            visited = new boolean[m][n];
+            for(int i =0; i<m-1; i++){
+                for(int j=0; j<n-1; j++){
+                    if(map[i][j] != '0' && check(i,j)){
+                        ch = true;
+                    }
+                }
+            }
+            
+            if (!ch) break;
+            
+            remove();
+            move();
+        }
+        
+        return cnt;
+    }
+    
+    public boolean check(int i, int j){
+        char c = map[i][j];
+        
+        if (map[i+1][j] != c || map[i][j+1] != c || map[i+1][j+1] != c) return false;
+        
+        visited[i][j] = true;
+        visited[i+1][j] = true;
+        visited[i][j+1] = true;
+        visited[i+1][j+1] = true;
+        
+        return true;
+    }
+    
+    public void remove(){
+        for(int i =0; i<M; i++){
+            for(int j=0; j<N; j++){
+                if(visited[i][j]) {
+                    map[i][j] = '0';
+                    cnt++;
+                }
+            }
+        }
+    }
+
+    public void move(){
+        for(int j =0; j<N; j++){
+            List<Character> list = new ArrayList<>();
+            
+            for(int i =M-1; i>=0; i--){
+                if (map[i][j] != '0') list.add(map[i][j]);
+            }
+            
+            int a = M-1;
+            
+            for(int i=0; i<list.size(); i++){
+                map[a--][j] = list.get(i); 
+            }
+            
+            for(int i =a; i>=0; i--){
+                map[i][j] = '0';
+            }
+        }
+    }
 }
