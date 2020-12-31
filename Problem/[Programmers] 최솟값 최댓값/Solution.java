0d33da4434c9422063fae9b25cd834f809849217
GooWeonKang <fkdldjs487@gmail.com>
2020-12-31 21:10:18 +0900

    프로그래머스 레벨2

---
+++ Problem/[Programmers] 최솟값 최댓값/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] str = s.split(" ");
+        int[] arr = new int[str.length];
+        for(int i=0; i<str.length; i++){
+            arr[i] = Integer.parseInt(str[i]);
+        }
+        
+        Arrays.sort(arr);
+        
+        return String.valueOf(arr[0]) + " " + String.valueOf(arr[arr.length-1]);
+    }
+}
